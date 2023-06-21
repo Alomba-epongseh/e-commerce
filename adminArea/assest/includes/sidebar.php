1c464024be365bc5f8f0b3656c021861503852b5
Alomba-epongseh <alombangwingche@gmail.com>
2023-06-21 17:05:58 +0100

    m

---
+++ adminArea/assest/includes/sidebar.php
@@ -0,0 +1,67 @@
+<!--**********************************
+            Sidebar start
+***********************************-->
+        <div class="dlabnav">
+            <div class="dlabnav-scroll">
+				<ul class="metismenu" id="menu">
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-home"></i>
+							<span class="nav-text">Insert Product</span>
+						</a>
+                    </li>
+					
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+						<i class="fas fa-info-circle"></i>
+							<span class="nav-text">View Product</span>
+						</a>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-chart-line"></i>
+							<span class="nav-text">Insert Blog</span>
+						</a>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fab fa-bootstrap"></i>
+							<span class="nav-text">View Blog</span>
+						</a>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-heart"></i>
+							<span class="nav-text">All Payment</span>
+						</a>
+                    </li>
+                    <li><a href="widget-basic.html" class="" aria-expanded="false">
+							<i class="fas fa-user-check"></i>
+							<span class="nav-text">List Users</span>
+						</a>
+					</li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-file-alt"></i>
+							<span class="nav-text">Logout</span>
+						</a>
+                    </li>
+                </ul>
+				<div class="side-bar-profile">
+					<div class="d-flex align-items-center justify-content-between mb-3">
+						<div class="side-bar-profile-img">
+							<img src="images/user.jpg" alt="">
+						</div>
+						<div class="profile-info1">
+							<h4 class="fs-18 font-w500">Vanilla</h4>
+							<span>alombangwingche@gmail.com</span>
+						</div>
+						<div class="profile-button">
+							<i class="fas fa-caret-down scale5 text-light"></i>
+						</div>
+					</div>	
+				</div>
+				
+				<div class="copyright">
+					<p><strong>Nilla Admin</strong> © 2023 All Rights Reserved</p>
+					<p class="fs-12">Made with <span class="heart"></span> by Nilla</p>
+				</div>
+			</div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
--- adminArea/includes/layouts/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Database Constants
-	define("DB_SERVER", 'localhost');
-	define("DB_USER", 'root');
-	define("DB_PASS", 'templenoir@yahoo1');
-	define("DB_NAME", 'cancermedz');
-?>
