e7db1f9c9e0bd837b28fbc97d26d5f8bbab13171
Alomba-epongseh <alombangwingche@gmail.com>
2023-06-22 17:48:38 +0100

    updated

--- website/function.php
+++ website/function.php
@@ -152,95 +152,6 @@ function getPro(){
 
 
 //function to get product details for a particular product from database and submit to website on click 
-function proDet(){
-
-    global $conn;
-
-    if (isset($_GET['prod_id'])) {
-    $product_id = $_GET['prod_id'];
-    $get_pro = "SELECT * FROM product where prodid = $product_id";
-    $run_pro = mysqli_query($conn, $get_pro);
-
-    while ($row_pro = mysqli_fetch_array($run_pro)) {
-        $pro_id  = $row_pro['prodid'];
-        $pro_name  = $row_pro['name'];
-        $pro_image  = $row_pro['image'];
-        $pro_price = $row_pro['price'];
-        $pro_desc = $row_pro['description'];
-
-                echo "
-                <div class='col-xl-7 col-lg-6'>
-                        <div class='product-details-content-area product-details--golden' data-aos='fade-up'
-                            data-aos-delay='200'>
-                            <!-- Start  Product Details Text Area-->
-                            <div class='product-details-text'>
-                                <h4 class='title'><a href='#'> $pro_name </a></h4>
-                                <div class='d-flex align-items-center'>
-                                    <ul class='review-star'>
-                                        <li class='fill'><i class='ion-android-star'></i></li>
-                                        <li class='fill'><i class='ion-android-star'></i></li>
-                                        <li class='fill'><i class='ion-android-star'></i></li>
-                                        <li class='fill'><i class='ion-android-star'></i></li>
-                                        <li class='empty'><i class='ion-android-star'></i></li>
-                                    </ul>
-                                    <a href='#' class='customer-review ml-2'>(customer review )</a>
-                                </div>
-                                <div class='price'>Price: $ $pro_price</div>
-                                <p> $pro_desc </p>
-                            </div> <!-- End  Product Details Text Area-->
-                            <!-- Start Product Variable Area -->
-                            <div class='product-details-variable'>
-                                <h4 class='title'>Available Options</h4>
-                                <!-- Product Variable Single Item -->
-                                <div class='variable-single-item'>
-                                    <div class='product-stock'> <span class='product-stock-in'><i
-                                                class='ion-checkmark-circled'></i></span> 200 IN STOCK</div>
-                                </div>
-                                <!-- Product Variable Single Item -->
-                                <div class='d-flex align-items-center '>
-                                    <div class='variable-single-item '>
-                                        <span>Quantity</span>
-                                        <div class='product-variable-quantity'>
-                                            <input min='1' max='100' value='1' type='number'>
-                                        </div>
-                                    </div>
-    
-                                    <div class='product-add-to-cart-btn'>
-                                        <a href='index-2.php?add_to_cart=$pro_id' class='btn btn-block btn-lg btn-black-default-hover'>+ Add To Cart</a>
-                                    </div>
-                                </div>
-                                <!-- Start  Product Details Meta Area-->
-                                <div class='product-details-meta mb-20'>
-                                    <a href='wishlist.html' class='icon-space-right'><i class='icon-heart'></i>Add to
-                                        wishlist</a>
-                                    <a href='compare.html' class='icon-space-right'><i class='icon-refresh'></i>Compare</a>
-                                </div> <!-- End  Product Details Meta Area-->
-                            </div> <!-- End Product Variable Area -->
-    
-                            <!-- Start  Product Details Social Area-->
-                            <div class='product-details-social'>
-                                <span class='title'>SHARE THIS PRODUCT:</span>
-                                <ul>
-                                    <li><a href='#'><i class='fa fa-facebook'></i></a></li>
-                                    <li><a href='#'><i class='fa fa-twitter'></i></a></li>
-                                    <li><a href='#'><i class='fa fa-pinterest'></i></a></li>
-                                    <li><a href='#'><i class='fa fa-google-plus'></i></a></li>
-                                    <li><a href='#'><i class='fa fa-linkedin'></i></a></li>
-                                </ul>
-                            </div> <!-- End  Product Details Social Area-->
-                        </div>
-                    </div>
-        
-        ";
-     };
-    };
-}
-
-
-
-
-
-
 function proDetails(){
 
     global $conn;
@@ -274,26 +185,62 @@ function proDetails(){
             </div>
             
             <div class='col-xl-7 col-lg-6'>
-                <div class='product-details-content-area product-details--golden' data-aos='fade-up' data-aos-delay='200'>
-                    <!-- Start  Product Details Text Area-->
-                    <div class='product-details-text'>
-                        <h4 class='title'>$pro_name</h4>
-                        <div class='d-flex align-items-center'>
-                            <ul class='review-star'>
-                                <li class='fill'><i class='ion-android-star'></i></li>
-                                <li class='fill'><i class='ion-android-star'></i></li>
-                                <li class='fill'><i class='ion-android-star'></i></li>
-                                <li class='fill'><i class='ion-android-star'></i></li>
-                                <li class='empty'><i class='ion-android-star'></i></li>
-                            </ul>
-                            <a href='#' class='customer-review ml-2'>(customer review )</a>
-                        </div>
-                        <div class='price'>$ $pro_price</div>
-                        <p>$pro_desc</p>
+                <div class='product-details-content-area product-details--golden' >
+                <!-- Start  Product Details Text Area-->
+                <div class='product-details-text'>
+                    <h4 class='title'>$pro_name</h4>
+                    <div class='d-flex align-items-center'>
+                        <ul class='review-star'>
+                            <li class='fill'><i class='ion-android-star'></i></li>
+                            <li class='fill'><i class='ion-android-star'></i></li>
+                            <li class='fill'><i class='ion-android-star'></i></li>
+                            <li class='fill'><i class='ion-android-star'></i></li>
+                            <li class='empty'><i class='ion-android-star'></i></li>
+                        </ul>
+                        <a href='#' class='customer-review ml-2'>(customer review )</a>
                     </div>
-                </div>
+                    <div class='price'>$ $pro_price</div>
+                    <p>$pro_desc</p>
+                </div> <!-- End  Product Details Text Area-->
+                <div class='product-details-variable'>
+                    <h4 class='title'>Available Options</h4>
+                    <!-- Product Variable Single Item -->
+                    <div class='variable-single-item'>
+                        <div class='product-stock'> <span class='product-stock-in'><i class='ion-checkmark-circled'></i></span> 200 IN STOCK</div>
+                    </div>
+                    <!-- Product Variable Single Item -->
+                    <div class='d-flex align-items-center '>
+                        <div class='variable-single-item '>
+                            <span>Quantity</span>
+                            <div class='product-variable-quantity'>
+                                <input min='1' max='100' value='1' type='number'>
+                            </div>
+                        </div>
+    
+                        <div class='product-add-to-cart-btn'>
+                            <a href='index-2.php?add_to_cart=$pro_id' class='btn btn-block btn-lg btn-black-default-hover'>+ Add To Cart</a>
+                        </div>
+                    </div>
+                    <!-- Start  Product Details Meta Area-->
+                    <div class='product-details-meta mb-20'>
+                        <a href='wishlist.php' class='icon-space-right'><i class='icon-heart'></i>Add to Wishlist</a>
+                        <a href='compare.php' class='icon-space-right'><i class='icon-refresh'></i>Compare</a>
+                    </div> <!-- End  Product Details Meta Area-->
+                </div> <!-- End Product Variable Area -->
+                <!-- Start  Product Details Social Area-->
+                <div class='product-details-social'>
+                    <span class='title'>SHARE THIS PRODUCT:</span>
+                    <ul>
+                        <li><a href='#'><i class='fa fa-facebook'></i></a></li>
+                        <li><a href='#'><i class='fa fa-twitter'></i></a></li>
+                        <li><a href='#'><i class='fa fa-pinterest'></i></a></li>
+                        <li><a href='#'><i class='fa fa-google-plus'></i></a></li>
+                        <li><a href='#'><i class='fa fa-linkedin'></i></a></li>
+                    </ul>
+                </div> <!-- End  Product Details Social Area-->
             </div>
         </div>
+    
         ";
      };
     };
@@ -396,17 +343,75 @@ function cart(){
         global $conn;
         $ip_address = getIp();
         $get_product_ip = $_GET['add_to_cart'];
-        $select_query = 'SELECT * FROM `cart` WHERE ip_add = $ip_address AND prodid = $get_product_ip';
+        $select_query = "SELECT * FROM cartdetails WHERE ip_address='$ip_address' AND prodid='$get_product_ip'";
         $result_query = mysqli_query($conn, $select_query);
         $num_of_rows = mysqli_num_rows($result_query);
         if($num_of_rows>0){
-            echo '<sript>alert(Data already found in cart)</script>';
-            echo "<sript>window.open('index-2.php','_self')</script>";
+            echo "<script>alert('Data already found in cart');</script>";
+            echo "<script>window.open('index-2.php','_self');</script>";
         }
         else{
-            $insert_query = "INSERT INTO `cart` (prodid, ip_add, quantity) VALUES ('$get_product_ip', '$ip_address', 0)";
-            echo '<sript>alert(Data inserted successfully)</script>';
-            echo "<sript>window.open('index-2.php','_self')</script>";
+            $insert_query = "INSERT INTO cartdetails (prodid, ip_address, quantity) VALUES ($get_product_ip, '$ip_address', 0)";
+            $result_query = mysqli_query($conn, $insert_query);
+            //echo "$insert_query";
+            echo "<script>alert(' Data inserted successfully');</script>";
+            echo "<script>window.open('index-2.php','_self');</script>";
         };
     };
 }
+
+
+//function to get cart item number
+function cart_item(){
+    if(isset($_GET['add_to_cart'])){
+        global $conn;
+        $ip_address = getIp();
+        $select_query = "SELECT * FROM cartdetails WHERE ip_address='$ip_address'";
+        $result_query = mysqli_query($conn, $select_query);
+        $num_of_items = mysqli_num_rows($result_query);
+    }else{
+        global $conn;
+        $ip_address = getIp();
+        $select_query = "SELECT * FROM cartdetails WHERE ip_address='$ip_address'";
+        $result_query = mysqli_query($conn, $select_query);
+        $num_of_items = mysqli_num_rows($result_query);
+        };
+    echo $num_of_items;
+    };
+
+    //function to get total price
+    function total_cart_price(){
+        global $conn;
+        $ip_address = getIp();
+        $total_price = 0;
+        $cart_query = "SELECT * FROM cartdetails WHERE ip_address='$ip_address'";
+        $result_query = mysqli_query($conn, $cart_query);
+
+        while($row=mysqli_fetch_array($result_query)){
+            $product_id = $row['prodid'];
+            $select_product = "SELECT * FROM product WHERE prodid='$product_id'";
+            $result_product = mysqli_query($conn, $select_product);
+            while($row_product_price=mysqli_fetch_array($result_product)){
+                $product_price = array($row_product_price['price']);
+                $product_values = array_sum($product_price);
+                $total_price=$total_price+$product_values;
+            }
+        }
+        echo $total_price;
+    }
+
+    //function to remove data from cart
+    function remove_cart_data(){
+        global $conn;
+        if (isset($_POST['remove_cart'])) {
+            foreach($_POST['removeitem'] as $removed_id){
+                echo $removed_id;
+                $delete_query = "DELETE FROM cartdetails WHERE prodid=$removed_id";
+                $result_delete = mysqli_query($conn, $delete_query);
+                if ($result_delete) {
+                    echo "<script>window.open('cart.php','_self');</script>";
+                }
+            }
+        }
+    }
+    
